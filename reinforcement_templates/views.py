12543c6c571bb7917939f9047feb2b314ca5d262
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-06 11:00:18 -0500

    Reinforcement complete

--- reinforcement_templates/views.py
+++ reinforcement_templates/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def new_profile(request):
-    context = {}
+    list = ["email", "username", "pin", "website", "address", "alias"]
+    context = {'field_items': list}
     return HttpResponse(render(request, 'profiles/new.html', context))
